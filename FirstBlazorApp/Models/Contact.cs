d025b6dcb76e6cb62ecec54a4fae524dc71412e2
Ivan Blazun <87031660+ivanblazun@users.noreply.github.com>
2023-05-15 23:01:40 +0200

    Add project files.

---
+++ FirstBlazorApp/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstBlazorApp.Models
+{
+    [Serializable]
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+
+}
