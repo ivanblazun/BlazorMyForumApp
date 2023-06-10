93cbc5e783cb0f4cf1eea39feb231766934dcb8f
Ivan Blazun <87031660+ivanblazun@users.noreply.github.com>
2023-06-10 21:52:05 +0200

    Private added to users and profiles, themes image upload added, nav menu changed

--- FirstBlazorApp/Models/User.cs
+++ FirstBlazorApp/Models/User.cs
@@ -12,7 +12,7 @@ namespace ForumAdminPanel.Models
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
         public string Password { get; set; }
         public DateTime RegisteredDate { get; set; }
 
